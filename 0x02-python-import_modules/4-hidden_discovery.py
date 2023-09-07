6e48afebbf1236b3a6d467d6e4a864e7636b198f
Mohamed El-Zafarani <misha@MSI>
2023-09-07 11:14:30 +0200

    added the :

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
+if __name__ == "__main__":
     import hidden_4
     for i in range(len(dir(hidden_4))):
         if dir(hidden_4)[i][0] == '_' and dir(hidden_4)[i][1] == '_':
