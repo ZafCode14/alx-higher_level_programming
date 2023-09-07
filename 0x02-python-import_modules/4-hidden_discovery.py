d7b87fffa344da62c9374b952c8b2b9bd60ce11f
Mohamed El-Zafarani <misha@MSI>
2023-09-07 11:03:01 +0200

    manditory

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import hidden_4
+for i in range(len(dir(hidden_4))):
+    if dir(hidden_4)[i][0] == '_' and dir(hidden_4)[i][1] == '_':
+        continue
+    print(dir(hidden_4)[i])
