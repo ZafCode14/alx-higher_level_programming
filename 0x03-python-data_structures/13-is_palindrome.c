24c2f85876f1f65c9448953110b4889040b19d98
Mohamed El-Zafarani <misha@MSI>
2023-09-09 09:40:35 +0200

    13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to pointer to head
- *
- * Return: pointer to the first node of the reversed list
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *prev, *current, *next;
-
-	prev = NULL;
-	current = *head;
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	*head = prev;
-	return (*head);
-}
-
-/**
  * is_palindrome - checks if is palindrome
  * @head: pointer to pointer to head node
  *
@@ -57,4 +34,27 @@ int is_palindrome(listint_t **head)
 	}
 
 	return (1);
+}
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to pointer to head
+ *
+ * Return: pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *current, *next;
+
+	prev = NULL;
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (*head);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -8,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ *
  */
 typedef struct listint_s
 {
@@ -19,6 +19,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+listint_t *reverse_listint(listint_t **head);
 
 int is_palindrome(listint_t **head);
 
