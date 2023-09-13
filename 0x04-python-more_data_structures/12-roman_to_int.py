ccc17873acb2a6e28503543a2584c4d11a75ad3f
Mohamed El-Zafarani <misha@MSI>
2023-09-13 14:46:39 +0200

    fixes for 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dic = {"M": 1000, "D": 500, "C": 100,
-            "L": 50, "X": 10, "V": 5, "I": 1}
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    dic = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
 
     result = dic[roman_string[0]]
 
