a47e304045392ee8c2dd85ad20c4b4cd79586432
Mohamed El-Zafarani <misha@MSI>
2023-09-13 15:01:07 +0200

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,17 @@ def roman_to_int(roman_string):
         return 0
     dic = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
 
-    result = dic[roman_string[0]]
+    result = 0
+    prev_value = 0
 
-    for i in range(0, len(roman_string) - 1):
-        if dic[roman_string[i]] >= dic[roman_string[i + 1]]:
-            result = result + dic[roman_string[i + 1]]
+    for char in reversed(roman_string):
+        current_value = dic[char]
+
+        if current_value >= prev_value:
+            result += current_value
         else:
-            result = dic[roman_string[i + 1]] - result
+            result -= current_value
+
+        prev_value = current_value
 
     return result
