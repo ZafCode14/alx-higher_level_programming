e577688ab2849c93067ac717523b6cf2977f8dda
Mohamed El-Zafarani <misha@MSI>
2023-09-13 15:32:44 +0200

    13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
     sum1 = 0
     sum2 = 0
     for i in my_list:
-        sum1 += i[0] * i[1] 
+        sum1 += i[0] * i[1]
     for i in my_list:
         sum2 += i[1]
     return sum1 / sum2
