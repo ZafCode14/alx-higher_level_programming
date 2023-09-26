c65e4a367c7fc93cc70f1bc3cae1e7a965923f42
Mohamed El-Zafarani <misha@MSI>
2023-09-26 14:16:33 +0200

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Define a class Square"""
+
+
 class Square:
-    """Represent a square"""
+    """Initialize size"""
     def __init__(self, size=0):
-        """Initialize a new square
-        Args:
-            size (int): the size of the new square
-        """
         self.size = size
 
     @property
