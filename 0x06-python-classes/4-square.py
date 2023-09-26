88a708fc82d433b6394f7f0de6e805d08c8b63a2
Mohamed El-Zafarani <misha@MSI>
2023-09-26 14:17:22 +0200

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
         else:
             if value < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = value
+        self.__size = value
 
     def area(self):
         return self.size * self.size
