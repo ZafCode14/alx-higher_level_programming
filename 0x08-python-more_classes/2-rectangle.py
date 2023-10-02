70951da6597247b918dbc58f658680ebcc92c2c7
Mohamed El-Zafarani <misha@MSI>
2023-10-02 13:26:54 +0200

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,10 +15,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """retrieves the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """sets the width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -27,10 +29,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """retrieves the height"""
         return self.__height
 
     @width.setter
     def height(self, value):
+        """sets the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -38,9 +42,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """returns the area"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """returns the perimeter"""
         if self.__width == 0 or self.__height == 0:
             result = 0
         else:
