ad27adb97de7216840a2c02d3ba7b5a6e17f4da3
Mohamed El-Zafarani <misha@MSI>
2023-10-09 16:35:05 +0200

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Module with a function"""
 
 def inherits_from(obj, a_class):
+    """Function that returns True or False"""
     if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     return False
