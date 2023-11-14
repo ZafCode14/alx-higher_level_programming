4450e66470bc249904003758aa475adedc7e141c
ZafCode14 <zafarwp14@gmail.com>
2023-11-14 14:05:09 +0200

    mand

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Updates the score of Bob to "10" in the "second_table"
+UPDATE hbtn_0c_0.second_table
+SET score = 10
+WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all records with a score <= 5 in the "second_table"
+DELETE FROM hbtn_0c_0.second_table
+WHERE score <= 5;
