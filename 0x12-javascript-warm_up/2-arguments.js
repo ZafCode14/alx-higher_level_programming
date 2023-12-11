768bfa76b3d4260f7a4997e1b57ccb2f90d8bf11
ZafCode14 <zafarwp14@gmail.com>
2023-12-11 13:05:58 +0200

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const args = process.argv;
 const len = args.length - 2;
 
 if (len === 0) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (len === 1) {
   console.log('Argument found');
 } else {
