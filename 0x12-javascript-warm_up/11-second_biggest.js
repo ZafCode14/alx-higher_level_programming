67b583dc912b307907031f66287dc5bfac85fdef
ZafCode14 <zafarwp14@gmail.com>
2023-12-11 13:15:31 +0200

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  const sorted = args.sort();
+  const sorted = args.sort().map(Number);
   console.log(sorted[args.length - 2]);
 }
