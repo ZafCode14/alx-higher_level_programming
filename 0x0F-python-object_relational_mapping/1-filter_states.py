04ed0f20d6be96f4f5e2dfa06313471180848846
ZafCode14 <zafarwp14@gmail.com>
2023-12-15 06:21:10 +0200

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("\
             SELECT * FROM states\
-            WHERE name LIKE 'N%'\
-            ORDER BY states.id ASC;\
+            WHERE name LIKE BINARY'N%'\
             ")
 
     rows = cur.fetchall()
