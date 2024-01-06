a2ddf0e93ce0076ee68f70172b23c73ec044dc71
ZafCode14 <zafarwp14@gmail.com>
2024-01-06 16:23:19 +0200

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,6 +5,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get("https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2]))
+    r = requests.get(
+            "https://api.github.com/repos/{}/{}/commits".format(
+                argv[1], argv[2]))
+    commit = r.json()
     for i in range(10):
-        print(r.json()[i].get("sha") + ":", r.json()[i].get("commit").get("author").get("name"))
+        print(
+                commit[i].get("sha") + ":",
+                commit[i].get("commit").get("author").get("name"))
